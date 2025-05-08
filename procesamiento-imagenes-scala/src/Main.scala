cf87bbf028d2d59afa64e3ac703699b61dad5d73
zak-lsk <zakaria.lassakeur@edu.uah.es>
2025-05-08 17:15:13 +0200

    Main continuo.

--- procesamiento-imagenes-scala/src/Main.scala
+++ procesamiento-imagenes-scala/src/Main.scala
@@ -34,110 +34,112 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    val ruta = pedir_ruta()
-    val tryImagen: Try[ImageData] = leerImagenBMP(ruta)
+    while (true){
+      val ruta = pedir_ruta()
+      val tryImagen: Try[ImageData] = leerImagenBMP(ruta)
 
-    menu()
-    val opcion = StdIn.readInt()
-    opcion match {
-      case 1 => { //Caso Blanco y Negro
-        val tryImagenBN: Try[ImageData] = tryImagen match {
-          case Success(imagen) => Success(convertirABlancoYNegro(imagen))
-          case failure@Failure(_) => failure
+      menu()
+      val opcion = StdIn.readInt()
+      opcion match {
+        case 1 => { //Caso Blanco y Negro
+          val tryImagenBN: Try[ImageData] = tryImagen match {
+            case Success(imagen) => Success(convertirABlancoYNegro(imagen))
+            case failure@Failure(_) => failure
+          }
+
+          tryImagenBN match {
+            case Success(imagenBN) =>
+              val rutaSalida = "./img/bn_" + new java.io.File(ruta).getName
+              if (escribirImagenBMP(imagenBN, rutaSalida)) {
+                println(s"Imagen en blanco y negro guardada como: $rutaSalida")
+              } else {
+                println("Error al guardar la imagen")
+              }
+
+            case Failure(e) =>
+              println(s"Error al procesar la imagen: ${e.getMessage}")
+          }
         }
+        case 2 => { // Caso Pixelar
+          println("Opción 2 elegida")
+          println("Elija el tamaño de pixelado: ")
+          val tamPixelado = StdIn.readInt()
+          val tryImagenPixelada: Try[ImageData] = tryImagen match {
+            case Success(imagen) => Success(pixelar(imagen, tamPixelado))
+            case failure@Failure(_) => failure
+          }
 
-        tryImagenBN match {
-          case Success(imagenBN) =>
-            val rutaSalida = "./img/bn_" + new java.io.File(ruta).getName
-            if (escribirImagenBMP(imagenBN, rutaSalida)) {
-              println(s"Imagen en blanco y negro guardada como: $rutaSalida")
-            } else {
-              println("Error al guardar la imagen")
-            }
+          tryImagenPixelada match {
+            case Success(imagenPixelada) =>
+              //            if (imagenPixelada.pixeles.exists(_.isEmpty)) {
+              //              println("Error: Hay filas vacías en la imagen pixelada.")
+              //            }
+              //            println("No tiene filas vacías ")
+              //          println("Dimensiones imagen pixelada: " + imagenPixelada.ancho + "x" + imagenPixelada.alto)
+              val rutaSalida = "./img/pixelada_" + new java.io.File(ruta).getName
+              if (escribirImagenBMP(imagenPixelada, rutaSalida)) {
+                println(s"Imagen pixelada guardada como: $rutaSalida")
+              } else {
+                println("Error al guardar la imagen")
+              }
 
-          case Failure(e) =>
-            println(s"Error al procesar la imagen: ${e.getMessage}")
+            case Failure(e) =>
+              println(s"Error al procesar la imagen: ${e.getMessage}")
+          }
+
         }
-      }
-      case 2 => { // Caso Pixelar
-        println("Opción 2 elegida")
-        println("Elija el tamaño de pixelado: ")
-        val tamPixelado = StdIn.readInt()
-        val tryImagenPixelada: Try[ImageData] = tryImagen match {
-          case Success(imagen) => Success(pixelar(imagen, tamPixelado))
-          case failure@Failure(_) => failure
+        case 3 => { // Caso Filtrar Colores
+          println("Opción 3 elegida")
+          print("Elija el factor de magnitud: ")
+          val factorMagnitud = StdIn.readDouble()
+          print("Elija el umbral: ")
+          val umbral = StdIn.readInt()
+
+          val tryImagenFiltrada: Try[(ImageData, ImageData, ImageData, contadorColores)] = tryImagen match {
+            case Success(imagen) => Success(identificarColores(imagen, factorMagnitud, umbral))
+            case Failure(e) => Failure(new Exception("Error al procesar la imagen", e))
+          }
+
+          tryImagenFiltrada match {
+            case Success((imagenRoja, imagenVerde, imagenAzul, contador)) =>
+              val rutaBase = "./img/"
+              val nombreArchivo = new java.io.File(ruta).getName
+
+              val rutaRoja = rutaBase + "roja_" + nombreArchivo
+              val rutaVerde = rutaBase + "verde_" + nombreArchivo
+              val rutaAzul = rutaBase + "azul_" + nombreArchivo
+
+              if (escribirImagenBMP(imagenRoja, rutaRoja)) {
+                println(s"Imagen filtrada roja guardada como: $rutaRoja")
+              } else {
+                println("Error al guardar la imagen roja")
+              }
+
+              if (escribirImagenBMP(imagenVerde, rutaVerde)) {
+                println(s"Imagen filtrada verde guardada como: $rutaVerde")
+              } else {
+                println("Error al guardar la imagen verde")
+              }
+
+              if (escribirImagenBMP(imagenAzul, rutaAzul)) {
+                println(s"Imagen filtrada azul guardada como: $rutaAzul")
+              } else {
+                println("Error al guardar la imagen azul")
+              }
+
+            case Failure(e) =>
+              println(s"Error al procesar la imagen: ${e.getMessage}")
+          }
+
         }
-
-        tryImagenPixelada match {
-          case Success(imagenPixelada) =>
-//            if (imagenPixelada.pixeles.exists(_.isEmpty)) {
-//              println("Error: Hay filas vacías en la imagen pixelada.")
-//            }
-//            println("No tiene filas vacías ")
-//          println("Dimensiones imagen pixelada: " + imagenPixelada.ancho + "x" + imagenPixelada.alto)
-            val rutaSalida = "./img/pixelada_" + new java.io.File(ruta).getName
-            if (escribirImagenBMP(imagenPixelada, rutaSalida)) {
-              println(s"Imagen pixelada guardada como: $rutaSalida")
-            } else {
-              println("Error al guardar la imagen")
-            }
-
-          case Failure(e) =>
-            println(s"Error al procesar la imagen: ${e.getMessage}")
+        case 4 => { // Salir
+          println("Saliendo...")
+          System.exit(0)
         }
-
-      }
-      case 3 => { // Caso Filtrar Colores
-        println("Opción 3 elegida")
-        print("Elija el factor de magnitud: ")
-        val factorMagnitud = StdIn.readDouble()
-        print("Elija el umbral: ")
-        val umbral = StdIn.readInt()
-
-        val tryImagenFiltrada: Try[(ImageData, ImageData, ImageData, contadorColores)] = tryImagen match {
-          case Success(imagen) => Success(identificarColores(imagen, factorMagnitud, umbral))
-          case Failure(e) => Failure(new Exception("Error al procesar la imagen", e))
+        case _ => {
+          println("No se reconoce el caracter introducido")
+          System.exit(0) // Por ahora, hay que introducir el bucle
         }
-
-        tryImagenFiltrada match {
-          case Success((imagenRoja, imagenVerde, imagenAzul, contador)) =>
-            val rutaBase = "./img/"
-            val nombreArchivo = new java.io.File(ruta).getName
-
-            val rutaRoja = rutaBase + "roja_" + nombreArchivo
-            val rutaVerde = rutaBase + "verde_" + nombreArchivo
-            val rutaAzul = rutaBase + "azul_" + nombreArchivo
-
-            if (escribirImagenBMP(imagenRoja, rutaRoja)) {
-              println(s"Imagen filtrada roja guardada como: $rutaRoja")
-            } else {
-              println("Error al guardar la imagen roja")
-            }
-
-            if (escribirImagenBMP(imagenVerde, rutaVerde)) {
-              println(s"Imagen filtrada verde guardada como: $rutaVerde")
-            } else {
-              println("Error al guardar la imagen verde")
-            }
-
-            if (escribirImagenBMP(imagenAzul, rutaAzul)) {
-              println(s"Imagen filtrada azul guardada como: $rutaAzul")
-            } else {
-              println("Error al guardar la imagen azul")
-            }
-
-          case Failure(e) =>
-            println(s"Error al procesar la imagen: ${e.getMessage}")
-        }
-
-      }
-      case 4 => { // Salir
-        println("Saliendo...")
-        System.exit(0)
-      }
-      case _ => {
-        println("No se reconoce el caracter introducido")
-        System.exit(0) // Por ahora, hay que introducir el bucle
       }
     }
   }
