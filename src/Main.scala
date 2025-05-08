d7e11cabbc0f10f27059faf2882cc721b8f82b7a
ikm24 <iker.manzano@edu.uah.es>
2025-05-08 17:33:45 +0200

    Menu con bucle en cloud

--- src/Main.scala
+++ src/Main.scala
@@ -85,23 +85,23 @@ object Main {
 
 
   def main(args: Array[String]): Unit = {
-    val ruta = pedir_ruta()
-    val tryImagen: Try[ImageData] = leerImagenBMP(ruta)
+    while (true) {
+      val ruta = pedir_ruta()
+      val tryImagen: Try[ImageData] = leerImagenBMP(ruta)
 
-    menu()
-    var tipo_operacion = ""
-    val opcion = StdIn.readInt()
-    opcion match {
-      case 1 => { //Caso Blanco y Negro
-        tipo_operacion = "Blanco y Negro"
-        val tryImagenBN: Try[ImageData] = tryImagen match {
-          case Success(imagen) => Success(convertirABlancoYNegro(imagen))
-          case failure@Failure(_) => failure
-        }
+      menu()
+      var tipo_operacion = ""
+      val opcion = StdIn.readInt()
+      opcion match {
+        case 1 => { //Caso Blanco y Negro
+          tipo_operacion = "Blanco y Negro"
+          val tryImagenBN: Try[ImageData] = tryImagen match {
+            case Success(imagen) => Success(convertirABlancoYNegro(imagen))
+            case failure@Failure(_) => failure
+          }
 
-        tryImagenBN match {
-          case Success(imagenBN) =>
-            {
+          tryImagenBN match {
+            case Success(imagenBN) => {
               val rutaSalida = "./img/bn_" + new java.io.File(ruta).getName
               if (escribirImagenBMP(imagenBN, rutaSalida)) {
                 println(s"Imagen en blanco y negro guardada como: $rutaSalida")
@@ -109,110 +109,111 @@ object Main {
                 println("Error al guardar la imagen")
               }
 
-              if(preguntar_subir_cloud()){
+              if (preguntar_subir_cloud()) {
                 val nombreUsuario = preguntar_nombre_usuario()
                 manejarSubidaCloud(imagenBN, rutaSalida, tipo_operacion, nombreUsuario, None, None)
               }
             }
 
-          case Failure(e) =>
-            println(s"Error al procesar la imagen: ${e.getMessage}")
+            case Failure(e) =>
+              println(s"Error al procesar la imagen: ${e.getMessage}")
+          }
         }
-      }
-      case 2 => { // Caso Pixelar
-        tipo_operacion = "Pixelado"
-        println("Opción 2 elegida")
-        println("Elija el tamaño de pixelado: ")
-        val tamPixelado = StdIn.readInt()
-        val tryImagenPixelada: Try[ImageData] = tryImagen match {
-          case Success(imagen) => Success(pixelar(imagen, tamPixelado))
-          case failure@Failure(_) => failure
-        }
-
-        tryImagenPixelada match {
-          case Success(imagenPixelada) => {
-            val rutaSalida = "./img/pixelada_" + new java.io.File(ruta).getName
-            if (escribirImagenBMP(imagenPixelada, rutaSalida)) {
-              println(s"Imagen pixelada guardada como: $rutaSalida")
-            } else {
-              println("Error al guardar la imagen")
-            }
-            if(preguntar_subir_cloud()){
-              // Preguntar el nombre de usuario
-              val nombreUsuario = preguntar_nombre_usuario()
-              manejarSubidaCloud(imagenPixelada, rutaSalida, tipo_operacion, nombreUsuario, None, None)
-            }
+        case 2 => { // Caso Pixelar
+          tipo_operacion = "Pixelado"
+          println("Opción 2 elegida")
+          println("Elija el tamaño de pixelado: ")
+          val tamPixelado = StdIn.readInt()
+          val tryImagenPixelada: Try[ImageData] = tryImagen match {
+            case Success(imagen) => Success(pixelar(imagen, tamPixelado))
+            case failure@Failure(_) => failure
           }
 
-          case Failure(e) =>
-            println(s"Error al procesar la imagen: ${e.getMessage}")
-        }
-
-      }
-      case 3 => { // Caso Filtrar Colores
-        tipo_operacion = "Filtrar Colores"
-        println("Opción 3 elegida")
-        print("Elija el factor de magnitud: ")
-        val factorMagnitud = StdIn.readDouble()
-        print("Elija el umbral: ")
-        val umbral = StdIn.readInt()
-
-        val tryImagenFiltrada: Try[(ImageData, ImageData, ImageData, contadorColores)] = tryImagen match {
-          case Success(imagen) => Success(identificarColores(imagen, factorMagnitud, umbral))
-          case Failure(e) => Failure(new Exception("Error al procesar la imagen", e))
-        }
-
-        tryImagenFiltrada match {
-          case Success((imagenRoja, imagenVerde, imagenAzul, contador)) => {
-            val rutaBase = "./img/"
-            val nombreArchivo = new java.io.File(ruta).getName
-
-            val rutaRoja = rutaBase + "roja_" + nombreArchivo
-            val rutaVerde = rutaBase + "verde_" + nombreArchivo
-            val rutaAzul = rutaBase + "azul_" + nombreArchivo
-
-            if (escribirImagenBMP(imagenRoja, rutaRoja)) {
-              println(s"Imagen filtrada roja guardada como: $rutaRoja")
-            } else {
-              println("Error al guardar la imagen roja")
+          tryImagenPixelada match {
+            case Success(imagenPixelada) => {
+              val rutaSalida = "./img/pixelada_" + new java.io.File(ruta).getName
+              if (escribirImagenBMP(imagenPixelada, rutaSalida)) {
+                println(s"Imagen pixelada guardada como: $rutaSalida")
+              } else {
+                println("Error al guardar la imagen")
+              }
+              if (preguntar_subir_cloud()) {
+                // Preguntar el nombre de usuario
+                val nombreUsuario = preguntar_nombre_usuario()
+                manejarSubidaCloud(imagenPixelada, rutaSalida, tipo_operacion, nombreUsuario, None, None)
+              }
             }
 
-            if (escribirImagenBMP(imagenVerde, rutaVerde)) {
-              println(s"Imagen filtrada verde guardada como: $rutaVerde")
-            } else {
-              println("Error al guardar la imagen verde")
-            }
-
-            if (escribirImagenBMP(imagenAzul, rutaAzul)) {
-              println(s"Imagen filtrada azul guardada como: $rutaAzul")
-            } else {
-              println("Error al guardar la imagen azul")
-            }
-
-            if(preguntar_subir_cloud()){
-              // Preguntar el nombre de usuario
-              val nombreUsuario = preguntar_nombre_usuario()
-              // Para la imagen roja
-              manejarSubidaCloud(imagenRoja, rutaRoja, tipo_operacion, nombreUsuario, Some(factorMagnitud), Some(umbral))
-              // Para la imagen verde
-              manejarSubidaCloud(imagenVerde, rutaVerde, tipo_operacion, nombreUsuario, Some(factorMagnitud), Some(umbral))
-              // Para la imagen azul
-              manejarSubidaCloud(imagenAzul, rutaAzul, tipo_operacion, nombreUsuario, Some(factorMagnitud), Some(umbral))
-            }
+            case Failure(e) =>
+              println(s"Error al procesar la imagen: ${e.getMessage}")
           }
 
-          case Failure(e) =>
-            println(s"Error al procesar la imagen: ${e.getMessage}")
         }
+        case 3 => { // Caso Filtrar Colores
+          tipo_operacion = "Filtrar Colores"
+          println("Opción 3 elegida")
+          print("Elija el factor de magnitud: ")
+          val factorMagnitud = StdIn.readDouble()
+          print("Elija el umbral: ")
+          val umbral = StdIn.readInt()
 
-      }
-      case 4 => { // Salir
-        println("Saliendo...")
-        System.exit(0)
-      }
-      case _ => {
-        println("No se reconoce el carácter introducido")
-        System.exit(0)
+          val tryImagenFiltrada: Try[(ImageData, ImageData, ImageData, contadorColores)] = tryImagen match {
+            case Success(imagen) => Success(identificarColores(imagen, factorMagnitud, umbral))
+            case Failure(e) => Failure(new Exception("Error al procesar la imagen", e))
+          }
+
+          tryImagenFiltrada match {
+            case Success((imagenRoja, imagenVerde, imagenAzul, contador)) => {
+              val rutaBase = "./img/"
+              val nombreArchivo = new java.io.File(ruta).getName
+
+              val rutaRoja = rutaBase + "roja_" + nombreArchivo
+              val rutaVerde = rutaBase + "verde_" + nombreArchivo
+              val rutaAzul = rutaBase + "azul_" + nombreArchivo
+
+              if (escribirImagenBMP(imagenRoja, rutaRoja)) {
+                println(s"Imagen filtrada roja guardada como: $rutaRoja")
+              } else {
+                println("Error al guardar la imagen roja")
+              }
+
+              if (escribirImagenBMP(imagenVerde, rutaVerde)) {
+                println(s"Imagen filtrada verde guardada como: $rutaVerde")
+              } else {
+                println("Error al guardar la imagen verde")
+              }
+
+              if (escribirImagenBMP(imagenAzul, rutaAzul)) {
+                println(s"Imagen filtrada azul guardada como: $rutaAzul")
+              } else {
+                println("Error al guardar la imagen azul")
+              }
+
+              if (preguntar_subir_cloud()) {
+                // Preguntar el nombre de usuario
+                val nombreUsuario = preguntar_nombre_usuario()
+                // Para la imagen roja
+                manejarSubidaCloud(imagenRoja, rutaRoja, tipo_operacion, nombreUsuario, Some(factorMagnitud), Some(umbral))
+                // Para la imagen verde
+                manejarSubidaCloud(imagenVerde, rutaVerde, tipo_operacion, nombreUsuario, Some(factorMagnitud), Some(umbral))
+                // Para la imagen azul
+                manejarSubidaCloud(imagenAzul, rutaAzul, tipo_operacion, nombreUsuario, Some(factorMagnitud), Some(umbral))
+              }
+            }
+
+            case Failure(e) =>
+              println(s"Error al procesar la imagen: ${e.getMessage}")
+          }
+
+        }
+        case 4 => { // Salir
+          println("Saliendo...")
+          System.exit(0)
+        }
+        case _ => {
+          println("No se reconoce el carácter introducido")
+          System.exit(0)
+        }
       }
     }
   }
